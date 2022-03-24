c9cdec114817bb048621870b5ba5c68a9f801bb0
yudichoccolatos <yudichoccolatos@gmail.com>
2022-03-24 17:51:18 +0700

    some message

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h>";
+		echo "<br>Nama Saya Wahyudi
+		<br>Saya tinggal didaerah Tangerang
+		<br>olah raga yang saya sukai adalah Berenang";
+	}
+}
+?>
---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Latihan 1</title>
+</head>
+<body>
+Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+Nilai 1 = <?= $nilai1; ?><br>
+Nilai 2 = <?= $nilai2; ?><br>
+ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
