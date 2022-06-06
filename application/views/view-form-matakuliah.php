d42c7daf2933bfc9116be99530a4f34cc4e1fed0
yudichoccolatos <yudichoccolatos@gmail.com>
2022-06-06 18:28:15 +0700

    UPLOAD AWAL

---
+++ application/views/view-form-matakuliah.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Form input Matakuliah</title>
+</head>
+<body>
+<center>
+<form action="<?= base_url('matakuliah/cetak'); ?>"
+method="post">
+            <table>
+                <tr>
+                    <th colspan="3">
+                        Form Input Data Mata Kuliah
+</th> </tr>
+                <tr>
+                    <td colspan="3">
+                    	<hr> </td>
+</tr> <tr>
+    <th>Kode MTK</th>
+    <th>:</th>
+    <td>
+        <input type="text" name="kode" id="kode">
+    </td>
+</tr> <tr>
+<th>Nama MTK</th>
+    <td>:</td>
+    <td>
+        <input type="text" name="nama" id="nama">
+    </td>
+</tr> <tr>
+    <th>SKS</th>
+    <td>:</td>
+    <td>
+        <select name="sks" id="sks">
+            <option value="">Pilih SKS</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+        </select>
+    </td>
+</tr> <tr>
+    <td colspan="3" align="center">
+        <input type="submit" value="Submit">
+</td>
+</tr>
+            </table>
+        </form>
+</body>
+</html>
